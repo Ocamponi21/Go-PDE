1fcf2f72de2f03a25f31f02c5bc2642a7d9dd2ca
Ocamponi21 <nicolasocampo288@gmail.com>
2024-04-29 08:43:55 -0400

    PDE

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-
+//this simple application demonstrates different components in Go
+// To start it is vital that we import the fmt library that hold the formatting for the print statements in Go
+// This function is iterating throught a series of number displaying them as they go.
+// When it reaches lucky number 4 it will display a special output.
+//This uses a combination of for loop and if statement to demonstarte simple decision structures
 func main() {
-	fmt.Println("Hello World!")
+	for i := 0; i < 5; i++ {
+		if i == 4 {
+			fmt.Println("Hello Lucky Number 4")
+		}
+		if i != 4 {
+			fmt.Print("Hello: ")
+			fmt.Println(i)
+		}
+	}
 }
